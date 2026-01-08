17b3f6f3d061ce3388a821fc1d3e260449078f86
JerichoFletcher <jericho.russel17@gmail.com>
2026-01-08 17:04:31 +0700

    feat: auto drive clock

--- sim/include/drawers/time_drawer.hpp
+++ sim/include/drawers/time_drawer.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <chrono>
+
 #include "imgui_layer.hpp"
 #include "verilated_container.hpp"
 
@@ -8,5 +10,21 @@ class TimeDrawer : public ImGuiDrawer {
     TimeDrawer(VerilatedContainer& v);
     void draw() override;
   private:
+    static constexpr int MAX_STEPS_PER_FRAME = 50'000;
+    using clock = std::chrono::steady_clock;
+  
     VerilatedContainer& m_v;
+    
+    bool m_auto;
+    bool m_prev_auto;
+    float m_clk_hz;
+    double m_tick_period;
+
+    bool m_step_req;
+    bool m_rst_req;
+
+    clock::time_point m_prev_time;
+    double m_accumulator;
+
+    void update();
 };
--- sim/src/drawers/time_drawer.cpp
+++ sim/src/drawers/time_drawer.cpp
@@ -1,6 +1,13 @@
 #include "drawers/time_drawer.hpp"
 
-TimeDrawer::TimeDrawer(VerilatedContainer& v): m_v(v) {}
+TimeDrawer::TimeDrawer(VerilatedContainer& v):
+  m_v(v),
+  m_auto(false),
+  m_prev_auto(false),
+  m_clk_hz(10.0f),
+  m_tick_period(0.1),
+  m_step_req(false),
+  m_rst_req(false) {}
 
 void TimeDrawer::draw() {
   ImGui::Begin("Time", nullptr,
@@ -11,12 +18,73 @@ void TimeDrawer::draw() {
   ImGui::Text("PC   : 0x%08x", this->m_v.pc());
 
   ImGui::Separator();
+  ImGui::SliderFloat(
+    "Clock (Hz)",
+    &this->m_clk_hz,
+    1.0f,
+    50'000'000.0f,
+    "%.0f",
+    ImGuiSliderFlags_Logarithmic
+  );
+  this->m_tick_period = 1.0 / (double)this->m_clk_hz;
+  ImGui::Text("Tick : %.9f s", this->m_tick_period);
+
+  ImGui::Separator();
+  ImGui::Checkbox("Auto", &this->m_auto);
   if (ImGui::Button("Advance")) {
-    this->m_v.tick();
+    this->m_auto = false;
+    this->m_step_req = true;
   }
   if (ImGui::Button("Reset")) {
-    this->m_v.reset();
+    this->m_auto = false;
+    this->m_rst_req = true;
   }
 
   ImGui::End();
+  this->update();
+}
+
+void TimeDrawer::update() {
+  if (this->m_rst_req) {
+    this->m_v.reset();
+    this->m_rst_req = false;
+    return;
+  }
+
+  if (!this->m_auto && !this->m_step_req) {
+    this->m_prev_auto = this->m_auto;
+    return;
+  }
+
+  if (this->m_auto && !this->m_prev_auto) {
+    this->m_prev_time = clock::now();
+    this->m_prev_auto = this->m_auto;
+  }
+
+  auto now = clock::now();
+  std::chrono::duration<double> delta = now - this->m_prev_time;
+  this->m_prev_time = now;
+
+  if (!this->m_auto && this->m_step_req) {
+    this->m_v.tick();
+    this->m_step_req = false;
+    this->m_accumulator = 0.0;
+    return;
+  }
+
+  this->m_accumulator += delta.count();
+  int steps = 0;
+  while (
+    this->m_accumulator >= this->m_tick_period
+    && steps < TimeDrawer::MAX_STEPS_PER_FRAME
+  ) {
+    this->m_v.tick();
+    this->m_accumulator -= this->m_tick_period;
+    steps++;
+  }
+
+  // Reset accumulator if max step count is reached
+  if (steps >= TimeDrawer::MAX_STEPS_PER_FRAME) {
+    this->m_accumulator = 0.0;
+  }
 }
